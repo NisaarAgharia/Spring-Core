663df1a86b82b0410d2fac629d2547dc90bda234
Nisaar <nagharia786@gmail.com>
2021-09-07 12:57:09 +0530

    BinarySearch

---
+++ src/main/java/com/example/scopes/BinarySearch.java
@@ -0,0 +1,63 @@
+package com.example.scopes;
+
+import java.util.Arrays;
+import java.util.OptionalInt;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+import javax.inject.Inject;
+import javax.inject.Named;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+@Named
+@Scope(value = ConfigurableBeanFactory.SCOPE_SINGLETON)
+public class BinarySearch {
+	
+	@Inject
+	@Qualifier("quick")
+	private SortAlgorithm sortAlgorithm;
+	
+	
+	
+	public SortAlgorithm getSortAlgorithm() {
+		return sortAlgorithm;
+	}
+
+	public void setSortAlgorithm(SortAlgorithm sortAlgorithm) {
+		this.sortAlgorithm = sortAlgorithm;
+	}
+
+	public int Search(int[] numbers, int key)
+	{
+		sortAlgorithm.sort(numbers);
+		
+		//
+		return Arrays.binarySearch(numbers, key);
+		
+	}
+
+	public OptionalInt sum(int[] numbers)
+	{
+		OptionalInt Sum =  Arrays.stream(numbers).reduce(Integer::sum);
+		
+		//
+		return Sum;
+		
+	}
+	
+	@PostConstruct
+	public void postConstruct() {
+		System.out.println("postConstruct");
+	}
+
+	@PreDestroy
+	public void preDestroy() {
+		System.out.println("preDestroy");
+	}
+
+}
