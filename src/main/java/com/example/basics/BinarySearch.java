e83ba1c847bbe3479abf590e89f10685871db272
Nisaar <nagharia786@gmail.com>
2021-09-08 13:47:11 +0530

    Spring BinarySearch

--- src/main/java/com/example/basics/BinarySearch.java
+++ src/main/java/com/example/basics/BinarySearch.java
@@ -5,9 +5,12 @@ import java.util.OptionalInt;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 @Component
+@Scope(value=ConfigurableBeanFactory.SCOPE_SINGLETON)
 public class BinarySearch {
 	
 	@Autowired
