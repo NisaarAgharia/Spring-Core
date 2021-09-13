0f54d9490ce3b5329fbc85cab4c6b3939113ea0f
Nisaar <nagharia786@gmail.com>
2021-09-13 12:32:22 +0530

    Sorting Algo

--- src/main/java/com/example/basics/BubbleSortAlgorithm.java
+++ src/main/java/com/example/basics/BubbleSortAlgorithm.java
@@ -2,9 +2,14 @@ package com.example.basics;
 
 import java.util.Arrays;
 
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 @Component
+@Qualifier("bubble")
+@Scope(value=ConfigurableBeanFactory.SCOPE_PROTOTYPE)
 public class BubbleSortAlgorithm implements SortAlgorithm{
 
 	@Override
