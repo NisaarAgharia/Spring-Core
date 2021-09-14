519fef095cd357988ac6bc22764021f6e0d8e406
Nisaar <nagharia786@gmail.com>
2021-09-14 18:59:05 +0530

    QUICK SORT ALGO

--- src/main/java/com/example/basics/QuickSortAlgorithm.java
+++ src/main/java/com/example/basics/QuickSortAlgorithm.java
@@ -3,12 +3,16 @@ package com.example.basics;
 import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
 import org.springframework.context.annotation.Primary;
+import org.springframework.context.annotation.Scope;
+import org.springframework.context.annotation.ScopedProxyMode;
 import org.springframework.stereotype.Component;
 
 @Component
 @Primary
 @Qualifier("quick")
+@Scope(value=ConfigurableBeanFactory.SCOPE_PROTOTYPE,proxyMode = ScopedProxyMode.TARGET_CLASS)
 public class QuickSortAlgorithm implements SortAlgorithm{
 
 	@Override
