483d8c0efa547d72a6cf5c50a64a1e847385a500
Nisaar <nagharia786@gmail.com>
2021-10-02 12:07:02 +0530

    JDBC connection

--- src/main/java/com/example/scopes/JdbcConnection.java
+++ src/main/java/com/example/scopes/JdbcConnection.java
@@ -1,11 +1,13 @@
 package com.example.scopes;
 
+import javax.inject.Named;
+
 import org.springframework.beans.factory.config.ConfigurableBeanFactory;
 import org.springframework.context.annotation.Scope;
 import org.springframework.context.annotation.ScopedProxyMode;
 import org.springframework.stereotype.Component;
 
-@Component
+@Named
 @Scope(value=ConfigurableBeanFactory.SCOPE_PROTOTYPE,proxyMode = ScopedProxyMode.TARGET_CLASS)
 public class JdbcConnection {
 
